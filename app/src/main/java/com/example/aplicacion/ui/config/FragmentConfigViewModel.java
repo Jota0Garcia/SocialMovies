32c64cc4f74bfc37c6d608a399861a3762de51db
alegrandoi <zzalejandro_3@hotmail.com>
2022-04-05 22:55:29 +0200

    menuFuncionando

--- app/src/main/java/com/example/aplicacion/ui/config/FragmentConfigViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.aplicacion.ui.config;
-
-import androidx.lifecycle.ViewModel;
-
-public class FragmentConfigViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
